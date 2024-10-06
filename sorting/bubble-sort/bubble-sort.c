7e092bfc7a22ceaa674a070207068928779be4b5
Ben <38275848+benjsmyth@users.noreply.github.com>
2024-10-06 14:28:01 -0400

    Update bubble-sort.c

--- sorting/bubble-sort/bubble-sort.c
+++ sorting/bubble-sort/bubble-sort.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include "../sorting.h"
 
-void bubbleSort(int size, int* array) {
+void bubbleSort(int size, int* ints) {
   int swapped;
   do {
     swapped = 0;
     for (int i = 0; i < size - 1; i++) {
-      if (array[i] > array[i + 1]) {
-        swap(i, i+1, array);
+      if (ints[i] > ints[i + 1]) {
+        swap(i, i+1, ints);
         swapped = 1;
       }
     }
