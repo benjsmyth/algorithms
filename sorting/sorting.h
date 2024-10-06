2666067002696d5cebd1aa8fabdc8a491a9c6477
Ben <jbensmyth@gmail.com>
2024-10-06 08:40:40 -0400

    Clean code

--- sorting/sorting.h
+++ sorting/sorting.h
@@ -1,18 +1,21 @@
-int* readInts(int argc, char* argv[]) {
-  int* array = (int*)malloc(sizeof(int) * (argc-1));
-  for (int i=1; i < argc; i++)
-    array[i-1] = atoi(argv[i]);
-  return array;
-}
+#include <stdio.h>
+#include <stdlib.h>
 
 void print(int* array, int size) {
-  for (int i = 0; i < size; i++)
+  for (int i=0; i < size; i++)
     printf("%d\n", array[i]);
   return;
 }
 
-void swap(int i, int* array) {
+int* readInts(int count, char* strings[]) {
+  int* ints = malloc( sizeof(int) * (count-1) );
+  for (int i=1; i < count; i++)
+    ints[i-1] = atoi(strings[i]);
+  return ints;
+}
+
+void swap(int i, int j, int* array) {
   int temp = array[i];
-  array[i] = array[i+1];
-  array[i+1] = temp;
+  array[i] = array[j];
+  array[j] = temp;
 }
