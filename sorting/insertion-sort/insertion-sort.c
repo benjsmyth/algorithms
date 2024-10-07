91d79b0f1519645d2e2e9069b125ff646f7ea52b
Ben <jbensmyth@gmail.com>
2024-10-06 21:42:18 -0400

    Fix tabs

--- sorting/insertion-sort/insertion-sort.c
+++ sorting/insertion-sort/insertion-sort.c
@@ -3,8 +3,8 @@
 void insertionSort(int n, int* array) {
   for (int i = 1; i < n; i++)
     for (int j = i; j > 0; j--)
-			if (array[j] < array[j-1])
-      	swap(j, j-1, array);
+      if (array[j] < array[j-1])
+        swap(j, j-1, array);
 }
 
 int main(int argc, char* argv[]) {
