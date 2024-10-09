399de90e6bf20916eeb57fb99e9f5223350e7aaf
Ben <38275848+benjsmyth@users.noreply.github.com>
2024-10-09 15:41:53 -0400

    Fix typos

--- sorting/bubble-sort/bubble-sort.c
+++ sorting/bubble-sort/bubble-sort.c
@@ -38,7 +38,6 @@ void bubbleSort3(int array[], const int N) {
             }
         }
         n = sorted;
-        print(array, N);
     }
     while (n > 1);
 }
@@ -48,19 +47,19 @@ int main(int argc, char* argv[]) {
     int array[N], array2[N], array3[N];
 
     for (int i=1; i <= N; i++) {
-        const int element = atoi(argv[i]);
-        array[i-1] = element;
-        array2[i-1] = element;
-        array3[i-1] = element;
+        const int E = atoi(argv[i]);
+        array[i-1] = E;
+        array2[i-1] = E;
+        array3[i-1] = E;
     }
 
     bubbleSort(array, N);
     printf("bubbleSort:\n"); print(array, N);
 
-    bubbleSort(array2, N);
+    bubbleSort2(array2, N);
     printf("bubbleSort2\n"); print(array2, N);
 
-    bubbleSort(array3, N);
+    bubbleSort3(array3, N);
     printf("bubbleSort3\n"); print(array3, N);
 
     return 0;
