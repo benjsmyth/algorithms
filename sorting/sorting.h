813c9b223c322cdadc9edee34d353ed832d1129f
Ben <jbensmyth@gmail.com>
2024-10-09 15:18:33 -0400

    Remove parsing method

--- sorting/sorting.h
+++ sorting/sorting.h
@@ -1,13 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* parseInts(char* argv[], const int N) {
-    int* array = malloc( sizeof(int) * (N-1) );
-    for (int i=1; i < N+1; i++)
-        array[i-1] = atoi(argv[i]);
-    return array;
-}
-
 void print(int array[], const int N) {
     putchar('[');
     for (int i=0; i < N; i++)
