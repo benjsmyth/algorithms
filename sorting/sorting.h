c502d1226c62c8833a049fdbc4c278a1a7e08003
Ben <jbensmyth@gmail.com>
2024-10-08 21:39:18 -0400

    Remove length method

--- sorting/sorting.h
+++ sorting/sorting.h
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* parseInts(int size, char* argv[]) {
-  int* ints = malloc( sizeof(int) * (size - 1) );
-  for (int i = 1; i < size + 1; i++)
-    ints[i - 1] = atoi(argv[i]);
-  return ints;
+int* parseInts(char* argv[], const int N) {
+    int* array = malloc( sizeof(int) * (N-1) );
+    for (int i=1; i < N+1; i++)
+        array[i-1] = atoi(argv[i]);
+    return array;
 }
 
-void print(int size, int* array) {
-  for (int i = 0; i < size; i++)
-    printf("%d ", array[i]);
-  printf("\n");
+void print(int array[], const int N) {
+    putchar('[');
+    for (int i=0; i < N; i++)
+        printf(i < N-1 ? "%d, " : "%d", array[i]);
+    printf("]\n");
 }
 
-void swap(int i, int j, int* array) {
-  int temp = array[i];
-  array[i] = array[j];
-  array[j] = temp;
+void swap(int array[], int i, int j) {
+    int temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
 }
