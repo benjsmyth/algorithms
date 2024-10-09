d9bb3876f49e2cd7cbf83cfcdb0fcd0da5da0430
Ben <38275848+benjsmyth@users.noreply.github.com>
2024-10-08 21:38:08 -0400

    Update sorting.h

--- sorting/sorting.h
+++ sorting/sorting.h
@@ -11,8 +11,7 @@ int* parseInts(char* argv[], const int N) {
 void print(int array[], const int N) {
     putchar('[');
     for (int i=0; i < N; i++)
-        printf(i < N-1 ? "%d, " : "%d", array[i]);
-    printf("]\n");
+        printf(i < N-1 ? "%d, " : "%d]\n", array[i]);
 }
 
 void swap(int array[], int i, int j) {
