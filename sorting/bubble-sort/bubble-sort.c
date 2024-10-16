fb0d9b06ad47525ee1b72edb5d2e9b3e1f753776
Ben <jbensmyth@gmail.com>
2024-10-15 21:02:22 -0400

    Move main back

--- sorting/bubble-sort/bubble-sort.c
+++ sorting/bubble-sort/bubble-sort.c
@@ -1,5 +1,6 @@
 #include "../sorting.h"
 #include <stdbool.h>
+#include <stdlib.h>
 
 void bubbleSort(int array[], const int N) {
     int n = 0;
@@ -40,4 +41,30 @@ void bubbleSort3(int array[], const int N) {
         n = sorted;
     }
     while (n > 1);
+}
+
+int main(int argc, char* argv[]) {
+    const int N = argc-1;
+    int array[N], array2[N], array3[N];
+
+    for (int i=0; i < N; i++) {
+        const int I = atoi(argv[i+1]);
+        array[i] = I;
+        array2[i] = I;
+        array3[i] = I;
+    }
+
+    bubbleSort(array, N);
+    printf("bubbleSort\n");
+    print(array, N);
+
+    bubbleSort2(array2, N);
+    printf("bubbleSort2\n");
+    print(array2, N);
+
+    bubbleSort3(array3, N);
+    printf("bubbleSort3\n");
+    print(array3, N);
+
+    return 0;
 }
