337317601c254af2babeec3156943c3573cbdb24
Ben <jbensmyth@gmail.com>
2024-10-15 20:53:54 -0400

    Isolate main code

--- sorting/bubble-sort/bubble-sort.c
+++ sorting/bubble-sort/bubble-sort.c
@@ -42,33 +42,3 @@ void bubbleSort3(int array[], const int N) {
     }
     while (n > 1);
 }
-
-int main(int argc, char* argv[]) {
-
-    // Size and arrays
-    const int N = argc-1;
-    int array[N], array2[N], array3[N];
-
-    // Read data into arrays
-    for (int i=0; i < N; i++) {
-        const int I = atoi(argv[i+1]);
-        array[i] = I;
-        array2[i] = I;
-        array3[i] = I;
-    }
-
-    // Sort arrays
-    bubbleSort(array, N);
-    printf("bubbleSort\n");
-    print(array, N);
-
-    bubbleSort2(array2, N);
-    printf("bubbleSort2\n");
-    print(array2, N);
-
-    bubbleSort3(array3, N);
-    printf("bubbleSort3\n");
-    print(array3, N);
-
-    return 0;
-}
