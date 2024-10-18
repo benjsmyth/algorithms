ab39f5dab884b7b2c4887e062713de484745931c
Ben <38275848+benjsmyth@users.noreply.github.com>
2024-10-17 20:22:29 -0400

    Increase indents

--- sorting/insertion-sort/insertion-sort.c
+++ sorting/insertion-sort/insertion-sort.c
@@ -1,28 +1,28 @@
 #include "../sorting.h"
 
 void insertionSort(int n, int* array) {
-  for (int i = 1; i < n; i++)
-    for (int j = i; j > 0; j--)
-      if (array[j] < array[j-1])
-        swap(j, j-1, array);
+    for (int i = 1; i < n; i++)
+        for (int j = i; j > 0; j--)
+            if (array[j] < array[j-1])
+                swap(j, j-1, array);
 }
 
 void insertionSortCopy(int n, int* array) {
-  for (int i = 1; i < n; i++) {
-    int x = array[i];
-    int j = i;
-    while ( (j > 0) && (x < array[j-1]) ) {
-      array[j] = array[j-1];
-      --j;
+    for (int i = 1; i < n; i++) {
+        int x = array[i];
+        int j = i;
+        while ( (j > 0) && (x < array[j-1]) ) {
+            array[j] = array[j-1];
+            j--;
+        }
+        array[j] = x;
     }
-    array[j] = x;
-  }
 }
 
 int main(int argc, char* argv[]) {
-  const int n = argc - 1;
-  int* array = parseInts(n, argv);
-  insertionSort(n, array);
-  print(n, array);
-  free(array);
+    const int n = argc - 1;
+    int* array = parseInts(n, argv);
+    insertionSort(n, array);
+    print(n, array);
+    free(array);
 }
